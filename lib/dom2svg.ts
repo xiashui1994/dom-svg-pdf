f6de080463211bac25514edd8b023de015c65a91
lijunchang <lijunchang@itixiao.com>
2024-07-16 22:41:42 +0800

    feat: bold

--- lib/dom2svg.ts
+++ lib/dom2svg.ts
@@ -5,6 +5,14 @@ export async function dom2svgString(element: HTMLElement): Promise<string> {
   const svgRootElement = svg.documentElement
   document.body.prepend(svgRootElement)
   try {
+    for (const text of Array.from(svgRootElement.querySelectorAll('text'))) {
+      const fontWeight = Number(text.getAttribute('font-weight')) || 400
+      if (fontWeight < 500)
+        continue
+      const color = text.getAttribute('color') || 'black'
+      text.setAttribute('stroke', color)
+      text.setAttribute('stroke-width', fontWeight > 500 ? '0.5' : '0.25')
+    }
     await inlineResources(svgRootElement, { cache: 'no-cache' })
   }
   finally {
