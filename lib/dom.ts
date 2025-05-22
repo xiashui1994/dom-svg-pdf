2c52431ff8d7200d9781be07bf7301c136d46ecd
lijunchang <lijunchang@itixiao.com>
2025-05-22 11:53:05 +0800

    fix: bold

--- lib/dom.ts
+++ lib/dom.ts
@@ -15,11 +15,13 @@ export function getSerializedElements(ctx: Window, el: string) {
 export function simulateBold(svgRootElement: HTMLElement) {
   for (const text of Array.from(svgRootElement.querySelectorAll('text'))) {
     const fontWeight = Number(text.getAttribute('font-weight')) || 400
-    if (fontWeight < 500)
-      continue
-    const color = text.getAttribute('fill') || 'black'
-    text.setAttribute('stroke', color)
-    text.setAttribute('stroke-width', fontWeight > 500 ? '0.5' : '0.25')
+    if (fontWeight >= 500) {
+      const color = text.getAttribute('fill') || 'black'
+      const strokeWidth = (fontWeight >= 700 ? 0.5 : 0.35).toFixed(2)
+      text.setAttribute('stroke', color)
+      text.setAttribute('stroke-width', strokeWidth)
+      text.setAttribute('fill-opacity', '0.85')
+    }
   }
 }
 
